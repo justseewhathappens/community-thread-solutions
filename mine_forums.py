26fe5ebfbf96ccadb88661fb44c868466006af4f
jw <jennie.wolfgang@gmail.com>
2018-04-08 17:09:41 -0500

    separate out list of messages into their own rows

--- mine_forums.py
+++ mine_forums.py
@@ -8,9 +8,18 @@ import numpy as np
 from tqdm import tqdm
 import io
 from filename import get_my_file_path
+import ast
 
 #define global variable for paths and data files
 folder_path = get_my_file_path()
+
+def splitListToRows(row,row_accumulator,target_column):
+    split_row = ast.literal_eval(row[target_column])
+    for s in split_row:
+        new_row = row.to_dict()
+        new_row[target_column] = s
+        row_accumulator.append(new_row)
+        
 
 def main():
     #start by loading all data from the folder of txt files
@@ -28,12 +37,30 @@ def main():
         with open(full_file_location, 'r') as f:
             str1 = f.read()
         doc_df = pd.read_csv(io.StringIO(str1))
-        print(doc_df.head())
+        #print(doc_df.head())
         thread_df = thread_df.append(doc_df, ignore_index= True)
         
     print ("Final threads df:")
     print (list(thread_df))
     #print (thread_df.head(n=5))
-    print (thread_df['Message HTML'][1])
+    #print (thread_df['Message HTML'][1])
+    
+    #Find how many threads are marked solved
+    num_solved = sum(thread_df['Solution Count']>0)
+    num_threads = len(thread_df)
+    percent_solved = (num_solved/num_threads)*100
+    print(f"{num_solved} of {num_threads} threads are solved, or {percent_solved}%")
+    
+    #create a dataframe of the messages, for tokenizing
+    message_df = thread_df[['Thread ID','Message Bodies']]
+    print (list(message_df))
+    print (message_df.head(n=20))
+    print (type(message_df['Message Bodies'][0]))
+
+    new_rows = []
+    target_column = 'Message Bodies'
+    message_df.apply(splitListToRows,axis=1,args = (new_rows,target_column))
+    new_df = pd.DataFrame(new_rows)
+    print (new_df.head(n=20))
         
 if __name__ == '__main__': main()
